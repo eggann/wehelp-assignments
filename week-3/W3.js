4820cdf293b58868ceb3fb6ae44cf66b10447326
eggann <59432847+eggann@users.noreply.github.com>
2022-01-14 19:21:55 +0800

    Add files via upload

--- week-3/W3.js
+++ week-3/W3.js
@@ -1,19 +1,14 @@
 let src="https://padax.github.io/taipei-day-trip-resources/taipei-attractions-assignment.json";
-// fetch(src).then(function(response){
-//     return response.json();
-// }).then(function(result){
-//     console.log("最終資料", result['result']['results'][0]);
-// });
 
 let content = document.createElement('div');
 content.setAttribute("class", "content");
 
 for(let i=1;i<9;i++){
-  console.log(i);
     
     let content2 = document.createElement('div');
     document.body.appendChild(content);
     content.appendChild(content2);
+    content2.setAttribute("class", "all");
 
     let img = document.createElement('img');
     document.body.appendChild(content);
@@ -33,17 +28,10 @@ for(let i=1;i<9;i++){
         const splits = myString.split(['https']);
         console.log(String("https")+splits[1]);
         img.setAttribute("src",String("https")+splits[1]);
-    });
+        img.setAttribute("class", "img");
 
-    fetch(src).then(function(response) {
-        return response.json();
-        })
-        .then(function(myJson) {
-        console.log(myJson['result']['results']['0']);
-        const myString_2 = myJson['result']['results'][i]['stitle'];
-        console.log(myString_2);
+        name.textContent = myJson['result']['results'][i]['stitle'];
         name.setAttribute("class", "img_name");
+
     });
-
-
-}
+};
