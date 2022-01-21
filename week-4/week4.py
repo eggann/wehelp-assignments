2201668e881a36a8f2507183835f0844d1d2650f
eggann <59432847+eggann@users.noreply.github.com>
2022-01-21 16:13:03 +0800

    Add files via upload

--- week-4/week4.py
+++ week-4/week4.py
@@ -1,7 +1,11 @@
-from flask import Flask
+from email import message
+from unittest import result
+from flask import Flask, url_for
 from flask import request # 載入 Request 物件
 from flask import render_template
 from flask import session
+from flask import redirect
+from markupsafe import string
 
 app = Flask(
     __name__,
@@ -9,8 +13,47 @@ app = Flask(
     static_url_path = "/" # 靜態檔案對應的網址路徑
     )
 
+app.secret_key="ant string but secret" # 設定 session 的密鑰
+
 @app.route("/")
 def index():
     return render_template("home.html")
+
+@app.route("/signin",methods=["POST"])
+def signin():
+    account=request.form["account"]
+    account=str(account)
+    session["account"]=account
+    
+    password=request.form["password"]
+    password=str(password)
+    session["password"]=password
+    
+    if session["account"]=="" or session["password"]=="":
+        result = "要填滿它哦"
+        return redirect(url_for('error', message=result))
+    elif session["account"] == session["password"] == "test":
+        return redirect("/member/")
+    else:
+        result = "填錯了哦"
+        return redirect(url_for('error', message=result))      
+     
+@app.route("/member/")
+def member():
+    return render_template("member.html")
+
+@app.route("/error/")
+def error():
+    if session["account"]=="" or session["password"]=="":
+        result = "請輸入帳號、密碼"
+        return render_template("error.html", data=result)
+    else:
+        result = "帳號、或密碼輸入錯誤"
+        return render_template("error.html", data=result)
+    
+@app.route("/signout")
+def signout():
+    session.pop('account', None)
+    return redirect('/')
 
 app.run(port=3000)
